1580362bde23ab1bea2cc9b3747c9d33eb75f76c
Junya Kosaka <kosakajunya@gmail.com>
2022-04-17 12:19:21 +0900

    feat: expressのcluster

--- development/backend/src/index.js
+++ development/backend/src/index.js
@@ -1,5 +1,5 @@
 var cluster = require('cluster');  
-var express = require('express');  
+// var express = require('express');  
 var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {  
@@ -8,135 +8,146 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 } else {
-  // Workers share the TCP connection in this server
-  var app = express();
+    const express = require('express')
+    const app = express();
+    
+    app.use(express.json({limit: '10mb'}))
+    
+    const api = require("./api");
+    
+    app.get('/api/hello', (req, res) => {
+      console.log('requested');
+      res.send({ response :'World!'})
+    })
+    
+    app.post('/api/client/records', async (req, res, next) => {
+      try {
+        await api.postRecords(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/records/:recordId', async (req, res, next) => {
+      try {
+        await api.getRecord(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/record-views/tomeActive', async (req, res, next) => {
+      try {
+        await api.tomeActive(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/record-views/allActive', async (req, res, next) => {
+      try {
+        await api.allActive(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/record-views/allClosed', async (req, res, next) => {
+      try {
+        await api.allClosed(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/record-views/mineActive', async (req, res, next) => {
+      try {
+        await api.mineActive(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.put('/api/client/records/:recordId', async (req, res, next) => {
+      try {
+        await api.updateRecord(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/records/:recordId/comments', async (req, res, next) => {
+      try {
+        await api.getComments(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.post('/api/client/records/:recordId/comments', async (req, res, next) => {
+      try {
+        await api.postComments(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/categories', async (req, res, next) => {
+      try {
+        await api.getCategories(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.post('/api/client/files', async (req, res, next) => {
+      try {
+        await api.postFiles(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/records/:recordId/files/:itemId', async (req, res, next) => {
+      try {
+        await api.getRecordItemFile(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    app.get('/api/client/records/:recordId/files/:itemId/thumbnail', async (req, res, next) => {
+      try {
+        await api.getRecordItemFileThumbnail(req, res);
+      } catch(e) {
+        console.log(e);
+        next(new Error("Unexpect"));
+      }
+    })
+    
+    
+    app.listen(8000, () => console.log('listening on port 8000...'))
+    // Workers share the TCP connection in this server
+    // var app = express();
 
-  app.use(express.json({limit: '10mb'}))
-  
-  const api = require("./api");
-  
-  app.get('/api/hello', (req, res) => {
-    console.log('requested');
-    res.send({ response :'World!'})
-  })
-  
-  app.post('/api/client/records', async (req, res, next) => {
-    try {
-      await api.postRecords(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/records/:recordId', async (req, res, next) => {
-    try {
-      await api.getRecord(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/record-views/tomeActive', async (req, res, next) => {
-    try {
-      await api.tomeActive(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/record-views/allActive', async (req, res, next) => {
-    try {
-      await api.allActive(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/record-views/allClosed', async (req, res, next) => {
-    try {
-      await api.allClosed(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/record-views/mineActive', async (req, res, next) => {
-    try {
-      await api.mineActive(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.put('/api/client/records/:recordId', async (req, res, next) => {
-    try {
-      await api.updateRecord(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/records/:recordId/comments', async (req, res, next) => {
-    try {
-      await api.getComments(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.post('/api/client/records/:recordId/comments', async (req, res, next) => {
-    try {
-      await api.postComments(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/categories', async (req, res, next) => {
-    try {
-      await api.getCategories(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.post('/api/client/files', async (req, res, next) => {
-    try {
-      await api.postFiles(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/records/:recordId/files/:itemId', async (req, res, next) => {
-    try {
-      await api.getRecordItemFile(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.get('/api/client/records/:recordId/files/:itemId/thumbnail', async (req, res, next) => {
-    try {
-      await api.getRecordItemFileThumbnail(req, res);
-    } catch(e) {
-      console.log(e);
-      next(new Error("Unexpect"));
-    }
-  })
-  
-  app.listen(8000, () => console.log('listening on port 8000...'))
+    // app.get('/', function (req, res) {
+    //     res.send('Hello World!');
+    // });
+    
+    // // All workers use this port
+    // app.listen(8080);
+}
 
-} 
+
